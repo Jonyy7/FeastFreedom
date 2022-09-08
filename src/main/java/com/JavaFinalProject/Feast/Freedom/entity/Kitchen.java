ef7d46a83268bbc7e62ab94b1d7365b786a01104
MichelleC-07 <michelle.chew.yan@gmail.com>
2022-09-08 16:03:33 -0500

    updated entity class

--- src/main/java/com/JavaFinalProject/Feast/Freedom/entity/Kitchen.java
+++ src/main/java/com/JavaFinalProject/Feast/Freedom/entity/Kitchen.java
@@ -5,7 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 //@Entity
 @Data
@@ -15,14 +16,26 @@ import javax.persistence.Entity;
 public class Kitchen {
 
 
+    @Id
+    @SequenceGenerator(
+            name = "kitchen_sequence",
+            sequenceName = "kitchen_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "kitchen_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private String password;
+    // TODO: figure out how to store image in sql database
     private String image;
-    private String workDays;
-    private String workTime;
-
-    public void add(){}
+    private String workDays; // "1110001"
+    @Temporal(TemporalType.TIME)
+    private Date workStartTime;
+    @Temporal(TemporalType.TIME)
+    private Date workEndTime;
 
 }
--- src/main/java/com/JavaFinalProject/Feast/Freedom/entity/Menu.java
+++ src/main/java/com/JavaFinalProject/Feast/Freedom/entity/Menu.java
@@ -1,4 +1,18 @@
 package com.JavaFinalProject.Feast.Freedom.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Menu {
+
+    private Long id;
+    private String name;
+    private String vegetarian;
+    private Double price;
 }
